28f484fbf2b41a9d8ad80f6a77db9cbbbc6dbd29
Alexander Bauer <sasha@crofter.org>
2014-09-06 01:49:11 -0400

    Implement UPC suggestions

--- foodd_main/urls.py
+++ foodd_main/urls.py
@@ -4,5 +4,7 @@ import views
 urlpatterns = urls.patterns(
   "",
   urls.url(r"^$", views.HomeView.as_view(), name="home"),
+  urls.url(r"^upc/suggest/(?P<upc>[0-9]{6,13})$", views.upc_suggest,
+      name="upc_suggest"),
   urls.url(r"^upc/info/(?P<upc>[0-9]{6,13})$", views.upc_info, name="upc_info")
 )
--- foodd_main/views.py
+++ foodd_main/views.py
@@ -1,5 +1,8 @@
 from django import http
 from django.views import generic
+from django import shortcuts
+from django.core import serializers
+import models
 import json
 import os
 import urllib
@@ -10,10 +13,23 @@ class HomeView(generic.TemplateView):
     template_name = "foodd_main/base.html"
 
 def upc_info(request, upc):
+    return http.HttpResponse(status=501)
+    return http.HttpResponse(serializers.serialize('json', item),
+            content_type='application/json')
+
+def upc_suggest(request, upc):
     # XXX: Look up in the local database.
 
     # Look up code in UPC database.
-    r = urllib.urlopen("http://api.upcdatabase.org/json/{}/{}".format(
-        UPC_APIKEY, upc))
+    r = json.loads(urllib.urlopen('http://api.upcdatabase.org/json/{}/{}' \
+            .format( UPC_APIKEY, upc)).read())
 
-    return http.HttpResponse(r, content_type="application/json")
+    info = {
+        'upc':         r['number'],
+        'description': r['description'],
+        'ingredient':  r['itemname'],
+        'size':        0
+    }
+
+    return http.HttpResponse(json.dumps(info),
+            content_type='application/json')
