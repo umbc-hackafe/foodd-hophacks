2a1aeff707e1ea1b2f43ed9eb887c517c439e3ab
Dylan Whichard <dylan@whichard.com>
2014-09-06 09:35:48 -0400

    Half working login page

--- app/foodd_main/urls.py
+++ app/foodd_main/urls.py
@@ -4,6 +4,9 @@ import foodd_main.views as views
 urlpatterns = urls.patterns(
   "",
   urls.url(r"^$", views.HomeView.as_view(), name="home"),
+  urls.url(r"^login/", "django.contrib.auth.views.login", {
+      "template_name": "foodd_main/login.html"
+  }),
   urls.url(r"^pantry/(?P<pk>[0-9]+)$", views.PantryView.as_view(), name="pantry"),
   urls.url(r"^item/(?P<ean>[0-9]{13})$", views.ItemView.as_view(), name="item"),
   urls.url(r"^ingredient$", views.IngredientsView.as_view(), name="ingredients"),
