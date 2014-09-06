f5c8cc9842ae2437cce15f9938bbe16f3140b6f8
Alexander Bauer <sasha@crofter.org>
2014-09-06 00:17:32 -0400

    Add upc/info/ URL and view

--- foodd_main/urls.py
+++ foodd_main/urls.py
@@ -4,4 +4,5 @@ import views
 urlpatterns = urls.patterns(
   '',
   urls.url(r'^$', views.home),
+  urls.url(r'^upc/info/(?P<upc>[0-9]{6,13})$', views.upc_info)
 )
--- foodd_main/views.py
+++ foodd_main/views.py
@@ -1,5 +1,21 @@
 from django import http
 from django import shortcuts
+import json
+import os
+from urllib import urlopen
+
+hosted_jquery = '<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>'
+
+UPC_APIKEY = os.getenv('FOODD_UPCDATABASE_KEY')
 
 def home(request):
     return shortcuts.render(request, "foodd_main/base.html")
+
+def upc_info(request, upc):
+    # XXX: Look up in the local database.
+
+    # Look up code in UPC database.
+    r = urlopen('http://api.upcdatabase.org/json/{}/{}'.format(
+        UPC_APIKEY, upc))
+
+    return http.HttpResponse(r, content_type='application/json')
