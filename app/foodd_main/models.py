d3fd919f67f33980fb0dfcbdfde068fd3db66d7f
Dylan Whichard <dylan@whichard.com>
2014-09-06 04:20:24 -0400

    Fix errors in models.py

--- app/foodd_main/models.py
+++ app/foodd_main/models.py
@@ -23,38 +23,41 @@ class Item(models.Model):
     size = models.IntegerField()
     description = models.CharField(max_length=128)
 
+class Pantry(models.Model):
+    owner = models.ForeignKey(contrib_models.User)
+    members = models.ManyToManyField(contrib_models.User, through='PantryMembership', related_name='+')
+    items = models.ManyToManyField(Item, through='PantryItem')
+
 class PantryMembership(models.Model):
     PANTRY_PERMISSIONS_CHOICES = (
         ('G', 'Guest'),
         ('M', 'Member'),
         ('O', 'Owner')
     )
+    pantry = models.ForeignKey(Pantry)
     user = models.ForeignKey(contrib_models.User)
     permissions = models.CharField(max_length=1, choices=PANTRY_PERMISSIONS_CHOICES)
 
 class PantryItem(models.Model):
+    pantry = models.ForeignKey(Pantry)
     item = models.ForeignKey(Item)
     remaining = models.IntegerField()
-
-class Pantry(models.Model):
-    owner = models.ForeignKey(contrib_models.User)
-    members = models.ManyToManyField(contrib_models.User, through='PantryMembership')
-    items = models.ManyToManyField(Item, through='PantryItem')
 
 class Modifier(models.Model):
     name = models.CharField(max_length=32)
 
+class Recipe(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient', related_name='fromrecipe')
+    preptime = models.IntegerField(null=True)
+
 class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe)
     ingredient = models.ForeignKey(Ingredient)
     amount = models.FloatField()
     forproperties = models.ManyToManyField(Property)
     required = models.BooleanField(default=True)
     modifiers = models.ManyToManyField(Modifier)
     grouping = models.IntegerField(null=True)
-
-class Recipe(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField()
-    instructions = models.TextField()
-    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
-    preptime = models.IntegerField(null=True)
