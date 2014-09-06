e9238e7eb2ab8da1d1e6d179785cbd752519ca42
Alexander Bauer <sasha@crofter.org>
2014-09-05 22:42:44 -0400

    Include JQuery in home page

--- foodd_main/views.py
+++ foodd_main/views.py
@@ -1,4 +1,6 @@
 from django import http
 
+hosted_jquery = '<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>'
+
 def home(request):
-    return http.HttpResponse('Hello World!')
+    return http.HttpResponse('Hello World!' + hosted_jquery)
