f921e787743b73e12584db27d1b1f9280db2973d
Dylan Whichard <dylan@whichard.com>
2014-09-06 01:38:22 -0400

    Add recipe-related models

--- foodd_main/models.py
+++ foodd_main/models.py
@@ -40,3 +40,21 @@ class Pantry(models.Model):
     owner = models.ForeignKey(contrib_models.User)
     members = models.ManyToManyField(contrib_models.User, through='PantryMembership')
     items = models.ManyToManyField(Item, through='PantryItem')
+
+class Modifier(models.Model):
+    name = models.CharField(max_length=32)
+
+class RecipeIngredient(models.Model):
+    ingredient = models.ForeignKey(Ingredient)
+    amount = models.FloatField()
+    forproperties = models.ManyToManyField(Property)
+    required = models.BooleanField(default=True)
+    modifiers = models.ManyToManyField(Modifier)
+    grouping = models.IntegerField(null=True)
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.ManyToManyField(Ingredient, through='RecipeIngredient')
+    preptime = models.IntegerField(null=True)
