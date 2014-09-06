b78873438b2b14b989bf23772b9b50f07b139c9f
Zachary Stewart <zstewar1@umbc.edu>
2014-09-06 14:00:02 -0400

    add and rename urls

--- app/foodd_main/api_urls.py
+++ app/foodd_main/api_urls.py
@@ -3,6 +3,6 @@ import foodd_main.api_views as api
 
 urlpatterns = urls.patterns(
   "",
-  urls.url(r"^ean/add$", api.EANAdd, name="add EAN"),
-  urls.url(r"^item/complete$", api.ItemComplete, name="complete item")
-)
+  urls.url(r"^ean/add$", api.EANAdd, name="EANAdd"),
+  urls.url(r"^item/complete$", api.ItemComplete, name="ItemComplete")
+  urls.url(r"^pantry/item/get$", api.PantryItemGet, name="PantryItemGet")
