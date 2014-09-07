711bf969487427b92abfa73977015aa3236d74f0
Dylan Whichard <dylan@whichard.com>
2014-09-07 00:34:42 -0400

    Fix stup stupid stupid balaoeasoeutnateh thing

--- app/static/js/pantry.js
+++ app/static/js/pantry.js
@@ -1,3 +1,17 @@
+function list_added_pantry_item(item) {
+    var outer = $("<div>", {"class": "list-group-item"});
+    outer.append($("<h4>", {"class": "list-group-item-heading", "type": "button",
+                            "data-toggle": "collapse", "data-target": "#ean-" + item.ean}).text("Name: " + item.name));
+    var inner = $("<div>", {"id": "ean-" + item.ean, "class": "collapse in"});
+    outer.append(inner);
+    if (item.remaining)
+	inner.append($("<p>").addClass("remaining").text("Number remaining: " + item.remaining));
+    if (item.item.size)
+	inner.append($("<p>").text("Size: " + item.item.size + " " + item.item.ingredient.unit));
+    $("#pantry-items").prepend(outer);
+    return outer;
+}
+
 $(document).ready(function() {
     $(document).keydown(function() {
 	if (!$(":focus").is(":input")) {
@@ -52,3 +66,10 @@ $(document).ready(function() {
 
     $("#ean").focus();
 });
+
+function removeFromPantry(ean) {
+    $.ajax({
+	"url": "/apy/v1/ingredient/",
+	"type": "delete"
+    });
+}
