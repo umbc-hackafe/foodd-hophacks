194f822c5ac465dfbd1dab9b50fd916eaf70bddf
Dylan Whichard <dylan@whichard.com>
2014-09-06 22:51:29 -0400

    Fix some more stupid pantry crap

--- app/foodd_main/api_views.py
+++ app/foodd_main/api_views.py
@@ -93,8 +93,8 @@ def PantryItemAdd(request):
         if inner_item.ingredient:
             dic["item"]["ingredient"]["unit"] = inner_item.ingredient.unit
 
-            dic["item"]["ingredient"]["provides"] = list(inner_item.ingredient.provides)
-            dic["item"]["ingredient"]["properties"] = list(inner_item.ingredient.properties)
+            dic["item"]["ingredient"]["provides"] = list(inner_item.ingredient.provides.all())
+            dic["item"]["ingredient"]["properties"] = list(inner_item.ingredient.properties.all())
     except models.Item.DoesNotExist:
         pass
 
