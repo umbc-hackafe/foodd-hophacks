6011515f945a2f4c6c5a426791479edfaaf590ef
Alexander Bauer <sasha@crofter.org>
2014-09-07 00:23:23 -0400

    Don't produce error on missing foodduser

--- app/foodd_main/context_processors.py
+++ app/foodd_main/context_processors.py
@@ -1,8 +1,11 @@
+from django.core import exceptions
 from foodd_main import models
 
 def find_foodduser(request):
     if request.user.is_authenticated():
-        foodduser = models.FooddUser.objects.get(user=request.user)
-        return { 'foodduser': foodduser }
+        try:
+            foodduser = models.FooddUser.objects.get(user=request.user)
+            return { 'foodduser': foodduser }
+        except exceptions.ObjectDoesNotExist: pass
 
     return {}
