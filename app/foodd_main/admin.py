5803aa7490405f8766aeb7dc9feb0aa3447d0c74
Dylan Whichard <dylan@whichard.com>
2014-09-06 20:57:41 -0400

    Add inline stuff to admin forms

--- app/foodd_main/admin.py
+++ app/foodd_main/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from foodd_main.models import FooddUser, Property, Ingredient, Item, Pantry, PantryMembership, PantryItem, Modifier, Recipe, RecipeIngredient
 
+class PantryItemInline(admin.TabularInline):
+    model = PantryItem
+
+class PantryAdmin(admin.ModelAdmin):
+    inlines = [PantryItemInline,]
+
 admin.site.register(FooddUser)
 admin.site.register(Property)
 admin.site.register(Ingredient)
 admin.site.register(Item)
-admin.site.register(Pantry)
-admin.site.register(PantryMembership)
+
 admin.site.register(PantryItem)
+admin.site.register(Pantry, PantryAdmin)
+
+admin.site.register(PantryMembership)
 admin.site.register(Modifier)
 admin.site.register(Recipe)
 admin.site.register(RecipeIngredient)
